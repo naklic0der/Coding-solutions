7e4e07c0fe1a5a577315ecd2c9824817c5deea82
naklic0der <yoyo71236@gmail.com>
2020-07-26 14:29:53 +0530

    26 jul 2020

---
+++ Codeforces Round #656 (Div. 3)/b.cpp
@@ -0,0 +1,119 @@
+/**
+ *    author:  nakli_c0der     
+**/
+///   TEMPLATE STARTED   /////
+
+///HEADER FILES///
+
+#include<bits/stdc++.h>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<cctype>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<string>
+#include<map>
+#include<set>
+#include<iostream>
+#include<algorithm>
+#include<sstream>
+#include<cstdlib>
+#include<complex>
+#include<iomanip>
+#include<utility>
+#include<bitset>
+#include<list>
+#include<assert.h>
+#include<numeric>
+#include<fstream>
+using namespace std;
+
+///DEFINES///
+
+#define ll long long 
+#define PP pair<ll, pair<ll, ll>>
+#define MOD ((ll int)(1e9+7))
+#define endl "\n"
+#define MAX(a,b) ( (a) > (b) ? (a) : (b))
+#define MIN(a,b) ( (a) < (b) ? (a) : (b))
+#define ALL(a) a.begin(), a.end()
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0)
+
+
+///TEMPLATES///
+template<class T1>void deb(T1 e)
+{
+    cout <<"-->"<< e << endl;
+}
+template<class T1, class T2> void deb(T1 e1, T2 e2)
+{
+    cout <<"-->"<< e1 << " " << e2 << endl;
+}
+template<class T1, class T2, class T3> void deb(T1 e1, T2 e2, T3 e3)
+{
+    cout <<"-->"<< e1 << " " << e2 << " " << e3 << endl;
+}
+template<class T1, class T2, class T3, class T4> void deb(T1 e1, T2 e2, T3 e3, T4 e4)
+{
+    cout <<"-->"<< e1 << " " << e2 << " " << e3 << " " << e4 << endl;
+}
+template<class T1, class T2, class T3, class T4, class T5> void deb(T1 e1, T2 e2, T3 e3, T4 e4, T5 e5)
+{
+    cout <<"-->"<< e1 << " " << e2 << " " << e3 << " " << e4 << " " << e5 << endl;
+}
+template<class T1, class T2, class T3, class T4, class T5, class T6> void deb(T1 e1, T2 e2, T3 e3, T4 e4, T5 e5, T6 e6)
+{
+    cout <<"-->"<< e1 << " " << e2 << " " << e3 << " " << e4 << " " << e5 << " " << e6 << endl;
+}
+
+///  TEMPLATE   OVER   ///
+
+///----------ANSWER -------------------------------///
+
+
+
+void Answer() {
+    
+	int n;
+	cin >> n;
+	n <<= 1;
+	vector<int> v;
+	for(int i=0; i<n; i++ ) {
+		int a;
+		cin >> a;
+		v.emplace_back(a);
+	}
+	reverse(v.begin(),v.end());
+	
+	int ar[n+10]={0};
+	
+	stack<int> st;
+	for(int x: v) {
+		if(ar[x] == 0) {
+			ar[x] = 1;
+			st.push(x);
+		}
+	}
+	
+	while(!st.empty()) {
+		cout << st.top() << ' ';
+		st.pop();
+	}
+	cout << endl;
+    
+}
+
+int main() {
+		
+    FASTIO;
+    
+    int t = 1;
+    cin>>t;
+    while(t--)
+    {
+        Answer();
+    }
+    return 0;
+}
